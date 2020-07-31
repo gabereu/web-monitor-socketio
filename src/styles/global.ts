febd0b639dfeae961485dfa014a89a45c6d61c5e
Gabriel Santos <gabereulive@hotmail.com>
2020-07-30 23:36:25 -0300

    Add Color Palette and Font Size

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,11 +3,22 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
     :root{
         --color: rgb(200,200,200);
-        --background: rgb(30,30,30);
-        --background-lighter: rgb(80,80,80);
+        --background: #322f3d;
+        --background-lighter: #4b5d67;
+        
+        --message-Emitted: #4b5d67;
+        --message-Received: #59405c;
+        --message-Monitoremitted: #87556f;
+
+        --color-1: #4b5d67;
+        --color-2: #322f3d;
+        --color-4: #87556f;
     }
     * {
         margin: 0;
-        padding: 0; 
+        padding: 0;
+    }
+    html,body{
+        font-size: 16px;
     }
 `;
